65fd725127c436a250d3f1f2791e58720680a8a0
Piyush Mehta <me@piyushmehta.com>
2019-06-23 23:51:22 +0530

    carousel

--- src/components/HeroSliderOne.js
+++ src/components/HeroSliderOne.js
@@ -1,72 +1,121 @@
-import React, {Component} from 'react';
-import Swiper from 'react-id-swiper';
+import React, { Component } from "react";
+import Swiper from "react-id-swiper";
 
-class HeroSliderOne extends Component{
-    
-    render(){
-        const params = {
-            slidesPerView : 1,
-            loop: true,
-            speed: 1000,
-            watchSlidesVisibility: true,
-            effect: 'fade',
-            navigation: {
-                nextEl: '.ht-swiper-button-next',
-                prevEl: '.ht-swiper-button-prev'
-            },
-            renderPrevButton: () => (
-                <div className="ht-swiper-button-prev ht-swiper-button-nav d-none d-xl-block"><i className="ion-ios-arrow-left" /></div>
-              ),
-              renderNextButton: () => (
-                <div className="ht-swiper-button-next ht-swiper-button-nav d-none d-xl-block"><i className="ion-ios-arrow-forward" /></div>
-              ),
-            autoplay: {
-                delay: 5000,
-            }
-        }
+class HeroSliderOne extends Component {
+  render() {
+    const params = {
+      slidesPerView: 1,
+      loop: true,
+      speed: 1000,
+      watchSlidesVisibility: true,
+      effect: "fade",
+      navigation: {
+        nextEl: ".ht-swiper-button-next",
+        prevEl: ".ht-swiper-button-prev"
+      },
+      renderPrevButton: () => (
+        <div className="ht-swiper-button-prev ht-swiper-button-nav d-none d-xl-block">
+          <i className="ion-ios-arrow-left" />
+        </div>
+      ),
+      renderNextButton: () => (
+        <div className="ht-swiper-button-next ht-swiper-button-nav d-none d-xl-block">
+          <i className="ion-ios-arrow-forward" />
+        </div>
+      ),
+      autoplay: {
+        delay: 5000
+      }
+    };
 
-        let data = [
-            {bgImg: 'slider3.jpg', sliderTitle: 'Build Your Dream With Passion', sliderSubtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore et dolore.', btnLink: 'contact-us'},
-            {bgImg: 'slider2.jpg', sliderTitle: 'Build Your Dream With Passion', sliderSubtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore et dolore.', btnLink: 'contact-us'},
-            {bgImg: 'slider1.jpg', sliderTitle: 'Build Your Dream With Passion', sliderSubtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore et dolore.', btnLink: 'contact-us'}
-        ];
+    let data = [
+      {
+        bgImg:
+          "https://image.freepik.com/free-photo/construction-site_53876-14088.jpg",
+        sliderTitle: "Build Your Dream With Passion",
+        sliderSubtitle:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore et dolore.",
+        btnLink: "contact-us"
+      },
+      {
+        bgImg:
+          "https://image.freepik.com/free-photo/industry-construction-site-labor-people-worker-wire-steel-bar_39730-1474.jpg",
+        sliderTitle: "Build Your Dream With Passion",
+        sliderSubtitle:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore et dolore.",
+        btnLink: "contact-us"
+      },
+      {
+        bgImg:
+          "https://image.freepik.com/free-photo/construction-silhouette_1150-8336.jpg",
+        sliderTitle: "Horizontal Framework",
+        sliderSubtitle:
+          "Horizontal formwork is used to temporarily support horizontal concrete work such as concrete slabs.",
+        btnLink: "Buy Now"
+      }
+    ];
 
-        let DataList = data.map((val, i)=>{
-            return(
-                
-                <div className="swiper-slide" key={i}>
-                    <div className="hero-slider__single-item" style={{ backgroundImage: `url(assets/img/slider/${val.bgImg})` }} >
-                        <div className="hero-slider__content-wrapper">
-                            <div className="container">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                <div className="hero-slider__content">
-                                    <h2 className="hero-slider__title">{val.sliderTitle}</h2>
-                                    <p className="hero-slider__text">{val.sliderSubtitle}</p>
-                                    <a className="hero-slider__btn" href={`${process.env.PUBLIC_URL}/${val.btnLink}`}> GET START</a>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
+    let DataList = data.map((val, i) => {
+      return (
+        <div className="swiper-slide" key={i}>
+          <div
+            className="hero-slider__single-item"
+            style={{ backgroundImage: `url(${val.bgImg})` }}
+          >
+            <div className="hero-slider__content-wrapper">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="hero-slider__content">
+                      <h2
+                        className="hero-slider__title"
+                        style={{
+                          boxShadow: "30px",
+                          textShadow: "2px 2px #111"
+                        }}
+                      >
+                        {val.sliderTitle}
+                      </h2>
+                      <p
+                        className="hero-slider__text"
+                        style={{
+                          boxShadow: "30",
+                          textShadow: "2px 2px #111"
+                        }}
+                      >
+                        {val.sliderSubtitle}
+                      </p>
+                      <a
+                        className="hero-slider__btn"
+                        href={`${process.env.PUBLIC_URL}/${val.btnLink}`}
+                        style={{
+                          boxShadow: "30",
+                          textShadow: "2px 2px #111"
+                        }}
+                      >
+                        {" "}
+                        Buy Now
+                      </a>
                     </div>
+                  </div>
                 </div>
-            )
-        });
-
-        return(
-            <div>
-                {/*====================  hero slider area ====================*/}
-                <div className="hero-alider-area">
-                    <Swiper {...params}>
-                        {DataList}
-                    </Swiper>
-                </div>
-                {/*====================  End of hero slider area  ====================*/}
-
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        {/*====================  hero slider area ====================*/}
+        <div className="hero-alider-area">
+          <Swiper {...params}>{DataList}</Swiper>
+        </div>
+        {/*====================  End of hero slider area  ====================*/}
+      </div>
+    );
+  }
 }
 
 export default HeroSliderOne;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import DemoIndex from "./demo_page/Demo";
 import HomeOne from "./HomeOne";
-import HomeTwo from "./HomeTwo";
-import About from "./pages/About";
-import Services from "./service/Services";
-import ServiceDetailsLeftSidebar from "./service/ServiceDetailsLeftSidebar";
-import ServiceDetailsRightSidebar from "./service/ServiceDetailsRightSidebar";
-import Projects from "./project/Projects";
-import ProjectDetails from "./project/ProjectDetails";
-import BlogLeftSidebar from "./blog/BlogLeftSidebar";
-import BlogRightSidebar from "./blog/BlogRightSidebar";
-import BlogDetailsLeftSidebar from "./blog/BlogDetailsLeftSidebar";
-import BlogDetailsRightSidebar from "./blog/BlogDetailsRightSidebar";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/404";
 import NoMAtch from "./pages/404";
@@ -39,6 +28,11 @@ class Root extends Component {
             exact
             path={`${process.env.PUBLIC_URL}/404`}
             component={PageNotFound}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/products`}
+            component={HomeOne}
           />
           <Route component={NoMAtch} />
         </Switch>
